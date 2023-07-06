7c84b6cf1d919565909c873e10a10f545c3be03a
Rafael Friedel <rfriedel@furb.br>
2023-07-05 21:32:12 -0300

    add functions to show text hovering over the title

---
+++ src/script.js
@@ -0,0 +1,9 @@
+function exibirTexto() {
+  var texto = document.getElementById("textoContratacao");
+  texto.style.display = "block";
+}
+
+function ocultarTexto() {
+  var texto = document.getElementById("textoContratacao");
+  texto.style.display = "none";
+}
